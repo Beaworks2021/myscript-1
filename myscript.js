f61265fc252f4974c0b815ff9ab94714221a72e2
Akaditi <you@example.com>
2021-07-07 14:27:13 +0000

    My first javascript exercise

---
+++ myscript.js
@@ -0,0 +1,26 @@
+
+
+
+
+let sum1 = 30 + 500
+console.log(sum1)
+
+const sum2 = 500 - 30
+console.log(sum2)
+
+var sum3 = 30 / 5
+console.log(sum3)
+
+const sum4 = (50 == 40)
+console.log(sum4)
+
+const sum5 = (50 >= 40)
+console.log(sum5)
+
+const sum6 = (50 != 50)
+console.log(sum6)
+
+const sum7 = (60 % 50)
+console.log(sum7)
+
+
